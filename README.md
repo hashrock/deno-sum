c1c9ffb9b1bfb5cd2e78e1b02dda424c371c721a
hashrock <hashrock@users.noreply.github.com>
2024-04-22 17:40:55 +0900

    add doc

---
+++ README.md
@@ -0,0 +1,1 @@
+This is a simple module with a single function that adds two numbers together.
--- mod.ts
+++ mod.ts
@@ -1,3 +1,15 @@
+/**
+ * This is a simple module with a single function that adds two numbers together.
+ * @module
+ */
+
+/**
+ * Add two numbers together.
+ * 
+ * @param a number to add
+ * @param b number to add to a
+ * @returns sum of a and b
+ */
 export function add(a: number, b: number): number {
   return a + b;
 }
